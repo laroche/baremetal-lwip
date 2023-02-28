6e3e4d4e6c815f3edadd64b771dd4945ed5e7fb7
Florian La Roche <Florian.LaRoche@gmail.com>
2023-02-28 22:23:46 +0100

    app.c: cleanup

--- app/app.c
+++ app/app.c
@@ -6,33 +6,31 @@
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-//versatilepb maps LAN91C111 registers here
-void * const eth0_addr = (void *) 0x10010000;
+/* versatilepb maps LAN91C111 registers here */
+static void * const eth0_addr = (void * const) 0x10010000UL;
 
-s_lan91c111_state sls = {.phy_address = 0,
+static s_lan91c111_state sls = {.phy_address = 0,
                          .ever_sent_packet = 0, 
                          .tx_packet = 0, 
                          .irq_onoff = 0};
 
-struct netif netif;
+static struct netif netif;
 
-//feed frames from driver to LwIP
-int
-process_frames(r16 * frame, int frame_len)
+/* feed frames from driver to LwIP */
+static int process_frames(r16 * frame, int frame_len)
 {
   struct pbuf* p = pbuf_alloc(PBUF_RAW, frame_len, PBUF_POOL);
-  if(p != NULL) {
+  if (p != NULL) {
     pbuf_take(p, frame, frame_len);
   }
-  if(netif.input(p, &netif) != ERR_OK) {
+  if (netif.input(p, &netif) != ERR_OK) {
     pbuf_free(p);
   }
   return 0;
 }
 
-//transmit frames from LwIP using driver
-static err_t 
-netif_output(struct netif *netif, struct pbuf *p)
+/* transmit frames from LwIP using driver */
+static err_t netif_output(struct netif *netif, struct pbuf *p)
 {
   unsigned char mac_send_buffer[p->tot_len];
   pbuf_copy_partial(p, (void*)mac_send_buffer, p->tot_len, 0);
@@ -40,8 +38,7 @@ netif_output(struct netif *netif, struct pbuf *p)
   return ERR_OK;
 }
 
-static err_t
-netif_set_opts(struct netif *netif)
+static err_t netif_set_opts(struct netif *netif)
 {
   netif->linkoutput = netif_output;
   netif->output     = etharp_output;
@@ -58,31 +55,25 @@ netif_set_opts(struct netif *netif)
   return ERR_OK;
 }
 
-int
-main(void)
+int main(void)
 {
-  ip4_addr_t addr;
-  ip4_addr_t netmask;
-  ip4_addr_t gw;
+  ip4_addr_t addr, netmask, gw;
 
   IP4_ADDR(&addr, 10, 0, 2, 99);
   IP4_ADDR(&netmask, 255, 255, 0, 0);
   IP4_ADDR(&gw, 10, 0, 0, 1);
 
   lwip_init();
-  netif_add(&netif, &addr, &netmask, &gw, 
-              NULL, netif_set_opts, netif_input);
-
+  netif_add(&netif, &addr, &netmask, &gw, NULL, netif_set_opts, netif_input);
   netif.name[0] = 'e';
   netif.name[1] = '0';
   netif_set_default(&netif);
   netif_set_up(&netif);
 
   nr_lan91c111_reset(eth0_addr, &sls, &sls);
-  nr_lan91c111_set_promiscuous(eth0_addr,&sls,1);
+  nr_lan91c111_set_promiscuous(eth0_addr, &sls, 1);
 
-  while(1) {
+  while (1) {
     nr_lan91c111_check_for_events(eth0_addr, &sls, process_frames);
   }
 }
-
